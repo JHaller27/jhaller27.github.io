38a77961056ad9344f135bc4430c5d5103b1b6d3
JHaller27 <JHaller27@users.noreply.github.com>
2017-08-07 13:15:49 -0400

    Create sites.js

---
+++ passwords/sites.js
@@ -0,0 +1,6 @@
+var sites = [ 
+   {seed:"amazon", url:"https://www.amazon.com/", displayName:"Amazon"}
+  ,{seed:"facebook", url:"https://www.facebook.com/", displayName:"Facebook"}
+  ,{seed:"google", url:"https://www.google.com/", displayName:"Google"}
+  ,{seed:"linkedin", url:"https://www.linkedin.com/", displayName:"LinkedIn"}
+];
